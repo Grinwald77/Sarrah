1c72f1ce6b5e210c53ec1dc629dc4277b8d0669e
Grinwald77 <123498323+Grinwald77@users.noreply.github.com>
2026-04-11 01:02:12 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 import { TopBlock } from './blocks/top.js';
-import { TableBlock } from './blocks/table.js';
+// import { TableBlock } from './blocks/table.js';
 import { AnalysisBlock } from './blocks/analysis.js';
 import { applyDir } from './i18n.js';
 
 function init(){
 
     TopBlock.init();
-    TableBlock.init();
+//    TableBlock.init();
     AnalysisBlock.init();
 
     applyDir();

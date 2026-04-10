2fdd47537523cd3a1ed7cb983699a45d8c81b1cd
Grinwald77 <123498323+Grinwald77@users.noreply.github.com>
2026-04-10 23:53:13 +0300

    Create store.js

---
+++ js/store.js
@@ -0,0 +1,19 @@
+export const Store = {
+    state:{
+        groups:[],
+        groupCount:5,
+        language:"en"
+    },
+    listeners:[],
+
+    set(key,val){
+        this.state[key]=val;
+        this.emit();
+    },
+
+    get(key){ return this.state[key]; },
+
+    subscribe(fn){ this.listeners.push(fn); },
+
+    emit(){ this.listeners.forEach(fn=>fn(this.state)); }
+};

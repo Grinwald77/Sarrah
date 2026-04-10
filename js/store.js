538b12852a8c46bc5d3dd80503ccc6fee69f4a8b
Grinwald77 <123498323+Grinwald77@users.noreply.github.com>
2026-04-11 01:16:05 +0300

    Update store.js

--- js/store.js
+++ js/store.js
@@ -1,19 +1,27 @@
 export const Store = {
+
     state:{
         groups:[],
         groupCount:5,
         language:"en"
     },
+
     listeners:[],
 
     set(key,val){
-        this.state[key]=val;
+        this.state[key] = val;
         this.emit();
     },
 
-    get(key){ return this.state[key]; },
+    get(key){
+        return this.state[key];
+    },
 
-    subscribe(fn){ this.listeners.push(fn); },
+    subscribe(fn){
+        this.listeners.push(fn);
+    },
 
-    emit(){ this.listeners.forEach(fn=>fn(this.state)); }
+    emit(){
+        this.listeners.forEach(fn => fn());
+    }
 };

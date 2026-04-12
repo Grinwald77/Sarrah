78694a4d4b82e0574c7a58d165a16abecc5ddb18
Grinwald77 <123498323+Grinwald77@users.noreply.github.com>
2026-04-12 22:18:49 +0300

    Add files via upload

--- js/blocks/top.js
+++ js/blocks/top.js
@@ -30,6 +30,9 @@ export const TopBlock = {
                     <option value="he">HE</option>
                 </select>
 
+                <span class="label">${t("groups")}</span>
+                <input id="groupCount" value="${state.groupCount}">
+
                 <span class="label">${t("periodType")}</span>
                 <select id="periodType">
                     <option value="months">${t("months")}</option>

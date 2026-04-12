b4b1a45ea404a792aa4682470998ab32a2930423
Grinwald77 <123498323+Grinwald77@users.noreply.github.com>
2026-04-12 21:58:05 +0300

    Update top.js

--- js/blocks/top.js
+++ js/blocks/top.js
@@ -30,8 +30,9 @@ export const TopBlock = {
                     <option value="he">HE</option>
                 </select>
 
-                <span class="label">${t("groups")}</span>
-                <input id="groupCount" value="${state.groupCount}">
+/* ===== удалил сам ===== */
+/                <span class="label">${t("groups")}</span>
+/               <input id="groupCount" value="${state.groupCount}">
 
                 <span class="label">${t("periodType")}</span>
                 <select id="periodType">

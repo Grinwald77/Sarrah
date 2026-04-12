e1834623048d0e3db55434069ad8cf1363e351d1
Grinwald77 <123498323+Grinwald77@users.noreply.github.com>
2026-04-12 22:17:04 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,21 +1,13 @@
 console.log("APP JS START");
 
 import { TopBlock } from './blocks/top.js';
-import { TopSecondaryBlock } from './blocks/top-secondary.js';
 import { TableBlock } from './blocks/table.js';
 import { AnalysisBlock } from './blocks/analysis.js';
 import { applyDir } from './i18n.js';
 
 function init(){
 
-    // ✅ store гарантированно создаётся ДО всех init блоков
-    window.store = {
-        activityName: '',
-        groupCount: 5
-    };
-
     TopBlock.init();
-    TopSecondaryBlock.init();
     TableBlock.init();
     AnalysisBlock.init();
 
